accde14db57f5556520d65b32e08a942e3e5920f
Szymon Cader <szymon.sc.cader@gmail.com>
2021-06-01 19:57:50 +0200

    Create server

---
+++ netime/server.py
@@ -0,0 +1,41 @@
+import random
+import time
+
+from netime import logger
+from netime.client import UDPClient
+
+
+class UDPServer(UDPClient):
+
+    def __init__(self, delay_std=0, delay_mean=0, loss_chance=0, *args, **kwargs):
+        self._delay_std = delay_std
+        self._delay_mean = delay_mean
+        self._loss_chance = loss_chance
+        self._addr = None
+        super().__init__(*args, **kwargs)
+
+    def bind(self, addr, port):
+        self._addr = (addr, port)
+        self._socket.bind(self._addr)
+
+    def wait(self):
+        value = random.gauss(self._delay_mean, self._delay_std)
+        logger.debug("Delaying packet for %s", value)
+        time.sleep(value)
+        return value
+
+    def skip_packet(self):
+        if random.random() <= self._loss_chance:
+            logger.debug("Packet skipped")
+            return True
+        return False
+
+    def run(self):
+        logger.info(f"Server started on {self._addr}")
+        while True:
+            data, cli_addr = self.receive()
+            logger.debug("Received packet of size %s", len(data))
+            if self.skip_packet():
+                continue
+            self.wait()
+            self.send(data.decode(), cli_addr)
